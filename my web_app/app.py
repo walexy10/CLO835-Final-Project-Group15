af2c0e9d3fd924460c5e0dfb43f6db4064ce451d
walexy10 <118873111+walexy10@users.noreply.github.com>
2023-04-20 03:38:16 -0400

    Update app.py

--- my web_app/app.py
+++ my web_app/app.py
@@ -10,7 +10,7 @@ app = Flask(__name__)
 
 DBHOST = os.environ.get("DBHOST") or "localhost"
 DBUSER = os.environ.get("DBUSER") or "root"
-DBPWD = os.environ.get("DBPWD")
+DBPWD = os.environ.get("DBPWD") or "password"
 DATABASE = os.environ.get("DATABASE") or "employees"
 COLOR_FROM_ENV = os.environ.get('APP_COLOR') or "lime"
 DBPORT = int(os.environ.get("DBPORT"))
